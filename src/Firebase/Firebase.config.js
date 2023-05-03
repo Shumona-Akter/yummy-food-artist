ccb6dda909b043edc6672992b3344b8fb5241c16
Shumona-Akter <shumona.cse.bd@gmail.com>
2023-05-03 11:59:09 +0600

    Environment variable to hide the Firebase config keys.

--- src/Firebase/Firebase.config.js
+++ src/Firebase/Firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDe0ABxspNpeWXK9hh0r4BoNU-NL0DLcUc",
-  authDomain: "yummy-food-artist.firebaseapp.com",
-  projectId: "yummy-food-artist",
-  storageBucket: "yummy-food-artist.appspot.com",
-  messagingSenderId: "77529569946",
-  appId: "1:77529569946:web:71bbfb6512d412ae645d23"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId
 };
 
 // Initialize Firebase
